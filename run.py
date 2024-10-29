8a60217782ae0649ff71c62c84a6da44c3ee94a9
shurvirarora <shurvirarora@gmail.com>
2024-10-29 16:36:52 +0800

    Update run.py for deployment

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
 
+# Initialize the Flask app using the factory function
 app = create_app()
 
 if __name__ == '__main__':
+    # Run the app locally with debug mode enabled
     app.run(debug=True)
